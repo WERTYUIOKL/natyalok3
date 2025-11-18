3a30976c15ad270c52025f3ce5cab283007f84a6
Ritish2-hash <garg47688@gmail.com>
2025-11-18 21:50:57 +0530

    Update errorMiddleware.js

--- middleware/errorMiddleware.js
+++ middleware/errorMiddleware.js
@@ -12,10 +12,11 @@
 //   };
   
 
-
+// Global error handler to manage both HTML and JSON responses
 export const errorHandler = (err, req, res, next) => {
     console.error(err.stack || err);
     const status = err.statusCode || 500;
+   // check if request expects an HTML response (browser)
     if (req.headers.accept && req.headers.accept.includes("text/html")) {
       return res.status(status).render("pages/error", { 
         message: err.message || "Server Error",
